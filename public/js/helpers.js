3d2c219dee0e9a65d7f5d68003cc0ce05e973586
Jamie Shepherd <email@jamie.sh>
2015-02-04 14:08:27 +0000

    Made nav resizable! Fixes #3

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -10,3 +10,34 @@ function generatePassword() {
 function addAttachment() {
     // Add attachment
 }
+
+function resizeNav() {
+
+    var nav = document.getElementById('nav');
+    var resizer = document.getElementsByClassName('resizer');
+    if(nav.classList.contains('large')) {
+        nav.className = 'large';
+    } else {
+        nav.className = 'small';
+    }
+
+    if(nav.className == 'large') {
+        nav.style.width = '80px';
+        document.getElementById('main').style.marginLeft = '80px';
+        var children = document.getElementsByClassName('resizable');
+        for(var i = 0; i < children.length; i++) {
+            children[i].style.display = 'none';
+        }
+        nav.className  = 'small';
+    } else {
+        nav.style.width = '300px';
+        document.getElementById('main').style.marginLeft = '300px';
+        var children = document.getElementsByClassName('resizable');
+        for(var i = 0; i < children.length; i++) {
+            children[i].style.display = 'inline';
+        }
+        nav.className  = 'large';
+
+    }
+
+}
--- resources/views/_layout/nav.blade.php
+++ resources/views/_layout/nav.blade.php
@@ -1,15 +1,16 @@
-<nav>
-    <img class="sponge-logo" src="/images/sponge-logo.svg">
+<nav id="nav" class="large">
+    <img class="sponge-logo resizable" src="/images/sponge-logo.svg">
     <ul>
         @if(Auth::user()->rank <= 2)
-            <a href="/dashboard"><li><i class="fa fa-desktop"></i><span>Dashboard</span></li></a>
-            <a href="/clients"><li><i class="fa fa-user"></i><span>Clients</span></li></a>
+            <a href="/dashboard"><li><i class="fa fa-desktop"></i><span class="resizable">Dashboard</span></li></a>
+            <a href="/clients"><li><i class="fa fa-user"></i><span class="resizable">Clients</span></li></a>
         @endif
         @if(Auth::user()->rank == 1)
-            <a href="/users"><li><i class="fa fa-users"></i><span>Users</span></li></a>
+            <a href="/users"><li><i class="fa fa-users"></i><span class="resizable">Users</span></li></a>
         @endif
-        <a href="/projects"><li><i class="fa fa-rocket"></i><span>Projects</span></li></a>
-        <a href="/account"><li><i class="fa fa-lock"></i><span>Account</span></li></a>
-        <a href="/auth/logout"><li><i class="fa fa-sign-out"></i><span>Sign out</span></li></a>
+        <a href="/projects"><li><i class="fa fa-rocket"></i><span class="resizable">Projects</span></li></a>
+        <a href="/account"><li><i class="fa fa-lock"></i><span class="resizable">Account</span></li></a>
+        <a href="/auth/logout"><li><i class="fa fa-sign-out"></i><span class="resizable">Sign out</span></li></a>
     </ul>
+    <div class="resizer" onClick="resizeNav()"></div>
 </nav>

94b3ff7f262c7a79c75164907dcd04625a3b1869
Jamie Shepherd <email@jamie.sh>
2015-02-06 17:32:24 +0000

    Fixed and tidied login errors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,12 +7,12 @@
         <form action="/auth/login" method="POST">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <label for="email">Email address</label>
-            <input name="email" type="text" placeholder="Email address" @if($errors->has('email')) class="error">
-            <span class="error">{{ $errors->first('email') }}</span> @else > @endif
+            <input name="email" type="text" placeholder="Email address">
             <label for="email">Password</label>
             <input name="password" type="password" placeholder="Password">
             <input name="remember" type="checkbox"><span class="remember">Remember me</span>
             <input type="submit" value="Sign in">
+            @if($errors->has()) <hr/><span class="error"><i class="fa fa-exclamation-triangle"></i> The details you entered were incorrect</span>@endif
         </form>
     </div>
 </div>

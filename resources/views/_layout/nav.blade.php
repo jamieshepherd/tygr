c78f11cc583ecd7bb0ecbc80bf4b07e6e61fb225
Jamie Shepherd <jamie.shepherd@outlook.com>
2015-02-11 12:49:18 +0000

    Made projects navigation only viewable for clients, fixes #45

--- resources/views/_layout/nav.blade.php
+++ resources/views/_layout/nav.blade.php
@@ -11,7 +11,9 @@
             @if(Auth::user()->rank == 1)
                 <a href="/users"><li><i class="fa fa-users"></i><span class="resizable">Users</span></li></a>
             @endif
+            @if(Auth::user()->rank == 3)
             <a href="/projects"><li><i class="fa fa-rocket"></i><span class="resizable">Projects</span></li></a>
+            @endif
             <a href="/account"><li><i class="fa fa-lock"></i><span class="resizable">Account</span></li></a>
             <a href="/auth/logout"><li><i class="fa fa-sign-out"></i><span class="resizable">Sign out</span></li></a>
         @endif

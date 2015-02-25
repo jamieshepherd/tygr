13ec36d9a78416d84c44dc4367f019ddb2c7973a
Jamie Shepherd <email@jamie.sh>
2015-02-25 14:06:19 +0000

    Added respond.js for old browser query support

--- resources/views/_layout/base.blade.php
+++ resources/views/_layout/base.blade.php
@@ -8,6 +8,7 @@
     <link media="all" type="text/css" rel="stylesheet" href="{{ elixir("css/main.css") }}">
     <link media="all" type="text/css" rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="/js/respond.min.js"></script>
     <script src="/js/helpers.js"></script>
     @yield('headlinks')
 </head>

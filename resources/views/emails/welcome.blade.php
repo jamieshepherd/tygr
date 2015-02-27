3d02e9ef9615d2563b9a138aae41f3d02cd9c546
Jamie Shepherd <jamie.shepherd@outlook.com>
2015-02-27 17:38:28 +0000

    Some more changes to mail config

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,7 +1,13 @@
-Hello {{ $name }}, you've been created as a user on the Sponge UK issue tracking system. You can use this area to log any amendments, changes, or issues with your projects as they are developed. Your login details are as follows:<br/>
-Email address: {{ $email }}<br/>
-Password: {{ $password }}
+<p><strong>Hello {{ $name }}, you've been created as a user on the Sponge UK issue tracking system.</strong></p>
 
-Please visit http://review.spongeuk.com to sign in.
+<p>You can use this area to log any amendments, changes, or issues with your projects as they are developed.</p>
 
-- Sponge UK
+<p>Your login details are as follows:</p>
+<ul>
+    <li>Email address: {{ $email }}</li>
+    <li>Password: {{ $password }}</li>
+</ul>
+
+<p>Please visit http://review.spongeuk.com to sign in.</p>
+
+<p><strong>- Sponge UK</strong></p>

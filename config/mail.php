869c0e7c8d00049eb48a5ab6590103a1b627700f
Jamie Shepherd <jamie.shepherd@outlook.com>
2015-02-27 17:34:54 +0000

    Finalised email settings

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
 	|
 	*/
 
-	'port' => env('MAIL_HOST', 587),
+	'port' => env('MAIL_PORT', 587),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => env('MAIL_FROM_EMAIL', null), 'name' => env('MAIL_FROM_NAME', null)],
 
 	/*
 	|--------------------------------------------------------------------------
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,7 @@
-Here is your html email.
-Your name is {{ $name }}<br/>
-Your email is {{ $email }}<br/>
-Your password is {{ $password }}
+Hello {{ $name }}, you've been created as a user on the Sponge UK issue tracking system. You can use this area to log any amendments, changes, or issues with your projects as they are developed. Your login details are as follows:<br/>
+Email address: {{ $email }}<br/>
+Password: {{ $password }}
+
+Please visit http://review.spongeuk.com to sign in.
+
+- Sponge UK
